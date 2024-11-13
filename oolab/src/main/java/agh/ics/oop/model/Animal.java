a461b6355a3d0427356be3f40b0295aa8155927c
jakubkalinski0 <kubakalinski2@gmail.com>
2024-11-13 05:03:29 +0100

    lab 4 do oecny

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,21 +1,21 @@
 package agh.ics.oop.model;
 
 public class Animal {
-    private MapDirection direction;
+    private MapDirection orientation;
     private Vector2d position;
 
     public Animal() {
-        this.direction = MapDirection.NORTH;
+        this.orientation = MapDirection.NORTH;
         this.position = new Vector2d(2,2);
     }
 
-    public Animal(MapDirection direction, Vector2d position) {
-        this.direction = direction;
+    public Animal(MapDirection orientation, Vector2d position) {
+        this.orientation = orientation;
         this.position = position;
     }
 
-    public MapDirection getDirection() {
-        return this.direction;
+    public MapDirection getOrientation() {
+        return this.orientation;
     }
 
     public Vector2d getPosition() {
@@ -24,33 +24,29 @@ public class Animal {
 
     @Override
     public String toString() {
-        return "Direction: " + this.direction + ", Position: " + this.position;
+        return this.orientation.toString();
     }
 
     public boolean isAt(Vector2d position) {
         return this.position.equals(position);
     }
 
-    public void move(MoveDirection direction) {
+    public void move(MoveDirection direction, RectangularMap map) {
         switch (direction) {
-            case LEFT -> this.direction = this.direction.previous();
-            case RIGHT -> this.direction = this.direction.next();
+            case LEFT -> this.orientation = this.orientation.previous();
+            case RIGHT -> this.orientation = this.orientation.next();
             case FORWARD -> {
-                Vector2d newPosition = this.position.add(this.direction.toUnitVector());
-                if (isWithinBounds(newPosition)) {
+                Vector2d newPosition = this.position.add(this.orientation.toUnitVector());
+                if (map.canMoveTo(newPosition)) {
                     this.position = newPosition;
                 }
             }
             case BACKWARD -> {
-                Vector2d newPosition = this.position.subtract(this.direction.toUnitVector());
-                if (isWithinBounds(newPosition)) {
+                Vector2d newPosition = this.position.subtract(this.orientation.toUnitVector());
+                if (map.canMoveTo(newPosition)) {
                     this.position = newPosition;
                 }
             }
         }
-    }
-
-    private boolean isWithinBounds(Vector2d position) {
-        return position.getX() >= 0 && position.getY() >= 0 && position.getX() <= 4 && position.getY() <= 4;
     }
 }
