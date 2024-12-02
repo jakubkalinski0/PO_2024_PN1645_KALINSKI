4ca2c582f9d1cf5fe252a32195c9a13c1b35b37b
jakubkalinski0 <kubakalinski2@gmail.com>
2024-12-02 03:56:17 +0100

    pula watkow

--- oolab/src/main/java/agh/ics/oop/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -2,14 +2,19 @@ package agh.ics.oop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class SimulationEngine {
     private final List<Simulation> simulations;
     private final List<Thread> threads;
+    private final ExecutorService threadPool;
 
     public SimulationEngine(List<Simulation> simulations) {
         this.simulations = simulations;
         this.threads = new ArrayList<>();
+        this.threadPool = Executors.newFixedThreadPool(4);
     }
 
     public void runSync() {
@@ -27,10 +32,21 @@ public class SimulationEngine {
         awaitSimulationsEnd();
     }
 
+    public void runAsyncInThreadPool() {
+        for (Simulation simulation : simulations) {
+            threadPool.execute(simulation);
+        }
+        awaitSimulationsEnd();
+    }
+
     public void awaitSimulationsEnd() {
         try {
             for (Thread thread : threads) {
                 thread.join();
+            }
+            threadPool.shutdown();
+            if (!threadPool.awaitTermination(10, TimeUnit.SECONDS)) {
+                threadPool.shutdownNow();
             }
         }
         catch (InterruptedException e) {
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -12,38 +12,39 @@ public class World {
     //f b r l f f r r f f f f f f f f
     public static void main(String[] args){
         try {
-//            List<MoveDirection> directions = OptionsParser.parse(args);
-//            List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
-//
-//            AbstractWorldMap map1 = new GrassField(10);
-//            AbstractWorldMap map2 = new RectangularMap(5, 5);
-//            map1.addObserver(new ConsoleMapDisplay());
-//            map2.addObserver(new ConsoleMapDisplay());
-//            Simulation simulation1 = new Simulation(positions, directions, map1);
-//            Simulation simulation2 = new Simulation(positions, directions, map2);
-//            SimulationEngine engine = new SimulationEngine(List.of(simulation1, simulation2));
+            List<MoveDirection> directions = OptionsParser.parse(args);
+            List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
+
+            AbstractWorldMap map1 = new GrassField(10);
+            AbstractWorldMap map2 = new RectangularMap(5, 5);
+            map1.addObserver(new ConsoleMapDisplay());
+            map2.addObserver(new ConsoleMapDisplay());
+            Simulation simulation1 = new Simulation(positions, directions, map1);
+            Simulation simulation2 = new Simulation(positions, directions, map2);
+            SimulationEngine engine = new SimulationEngine(List.of(simulation1, simulation2));
 //            engine.runSync();
 //            engine.runAsync();
+            engine.runAsyncInThreadPool();
 
-            List<MoveDirection> directions = parse(args);
-            List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
-            List<Simulation> simulations = new ArrayList<>();
-
-            for (int i = 0; i < 1000; i++) {
-                AbstractWorldMap map;
-                if (i%2 == 0) {
-                    map = new GrassField(10);
-                }
-                else {
-                    map = new RectangularMap(5,5);
-                }
-                map.addObserver(new ConsoleMapDisplay());
-                Simulation simulation = new Simulation(positions, directions, map);
-                simulations.add(simulation);
-            }
-
-            SimulationEngine engine = new SimulationEngine(simulations);
-            engine.runAsync();
+//            List<MoveDirection> directions = parse(args);
+//            List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+//            List<Simulation> simulations = new ArrayList<>();
+//
+//            for (int i = 0; i < 1000; i++) {
+//                AbstractWorldMap map;
+//                if (i%2 == 0) {
+//                    map = new GrassField(10);
+//                }
+//                else {
+//                    map = new RectangularMap(5,5);
+//                }
+//                map.addObserver(new ConsoleMapDisplay());
+//                Simulation simulation = new Simulation(positions, directions, map);
+//                simulations.add(simulation);
+//            }
+//
+//            SimulationEngine engine = new SimulationEngine(simulations);
+//            engine.runAsync();
         }
         catch (IllegalArgumentException e){
             System.out.println("Error: " + e.getMessage());
