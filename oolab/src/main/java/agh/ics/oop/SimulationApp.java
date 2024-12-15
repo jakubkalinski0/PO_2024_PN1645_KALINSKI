127da4839cd06fb2d4d1388bde0f06d1979a4e0a
jakubkalinski0 <kubakalinski2@gmail.com>
2024-12-15 03:03:41 +0100

    szkielet aplikacji JavaFX

---
+++ oolab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -0,0 +1,14 @@
+package agh.ics.oop;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SimulationApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Simulation");
+        primaryStage.show();
+    }
+}
