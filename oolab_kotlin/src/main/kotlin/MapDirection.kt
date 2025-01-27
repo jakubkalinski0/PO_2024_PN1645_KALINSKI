d38f7108c0302185680db85fd2f28906bfc71ced
jakubkalinski0 <kubakalinski2@gmail.com>
2025-01-27 15:14:37 +0100

    Added MoveDirection and refactorized MapDirection

--- oolab_kotlin/src/main/kotlin/MapDirection.kt
+++ oolab_kotlin/src/main/kotlin/MapDirection.kt
@@ -3,40 +3,31 @@ package agh.ics.oop.model
 enum class MapDirection {
     NORTH, EAST, SOUTH, WEST;
 
-    override fun toString(): String {
-        return when (this) {
-            NORTH -> "N"
-            EAST -> "E"
-            SOUTH -> "S"
-            WEST -> "W"
-        }
+    override fun toString(): String = when (this) {
+        NORTH -> "N"
+        EAST -> "E"
+        SOUTH -> "S"
+        WEST -> "W"
     }
 
-    fun next(): MapDirection {
-        return when (this) {
-            EAST -> SOUTH
-            SOUTH -> WEST
-            WEST -> NORTH
-            NORTH -> EAST
-        }
+    fun next(): MapDirection = when (this) {
+        EAST -> SOUTH
+        SOUTH -> WEST
+        WEST -> NORTH
+        NORTH -> EAST
     }
 
-    fun previous(): MapDirection {
-        return when (this) {
-            EAST -> NORTH
-            NORTH -> WEST
-            WEST -> SOUTH
-            SOUTH -> EAST
-        }
+    fun previous(): MapDirection = when (this) {
+        EAST -> NORTH
+        NORTH -> WEST
+        WEST -> SOUTH
+        SOUTH -> EAST
     }
 
-    // toUnitVector method
-    fun toUnitVector(): Vector2d {
-        return when (this) {
-            NORTH -> Vector2d(0, 1)
-            EAST -> Vector2d(1, 0)
-            SOUTH -> Vector2d(0, -1)
-            WEST -> Vector2d(-1, 0)
-        }
+    fun toUnitVector(): Vector2d = when (this) {
+        NORTH -> Vector2d(0, 1)
+        EAST -> Vector2d(1, 0)
+        SOUTH -> Vector2d(0, -1)
+        WEST -> Vector2d(-1, 0)
     }
 }
